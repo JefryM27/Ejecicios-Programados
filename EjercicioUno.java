8b58a3644637a65840d88e75933dcf7aeac1584f
JefryM27 <jefrymoreraespinoza09@gmail.com>
2023-05-17 11:59:47 -0600

    Ejercicio 1

---
+++ EjercicioUno.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class EjercicioUno{
+    public static void main(String[] args ){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Ingrese un numero");
+        int num = input.nextInt();
+        if (num%2==0){
+            System.out.println("Par");
+        }else{
+           System.out.println("Impar");
+        }
+    }
+
+}
