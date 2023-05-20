bede0e62e41418ae9909eb565d24254c101a130d
JefryM27 <jefrymoreraespinoza09@gmail.com>
2023-05-19 22:02:19 -0600

    Despedidada EjercicioDos

--- EjercicioDos.java
+++ EjercicioDos.java
@@ -8,5 +8,6 @@ public static void main(String[] args){
 double celsius = input.nextDouble();
 double farenheit = (9.0/5)*celsius+32;
 System.out.println("El resultado en grados Farenheit es: "+farenheit);
+System.out.println("!Adios¡");
 }    
 }
