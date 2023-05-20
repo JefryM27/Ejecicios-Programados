b5efd7d1ea510a7fea6af366312e019ca6ab33a8
JefryM27 <jefrymoreraespinoza09@gmail.com>
2023-05-19 22:00:02 -0600

    EjercicioDos

---
+++ EjercicioDos.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class EjercicioDos {
+public static void main(String[] args){
+    Scanner input = new Scanner (System.in);
+    System.out.println("Sistema de conversion de celsius");
+    System.out.println("Ingrese un numero en estado celsius: ");
+double celsius = input.nextDouble();
+double farenheit = (9.0/5)*celsius+32;
+System.out.println("El resultado en grados Farenheit es: "+farenheit);
+}    
+}
