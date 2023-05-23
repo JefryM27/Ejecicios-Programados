ec75a145d9a3938d63ad6a337ea00e8eebe6dd9b
JefryM27 <jefrymoreraespinoza09@gmail.com>
2023-05-22 22:48:35 -0600

    Ejercicio4

--- Ejercicio4.java
+++ Ejercicio4.java
@@ -2,26 +2,27 @@ import   java.util.Scanner;
 
 public class Ejercicio4{
     public static void main(String[] args) {
-        Scanner input = new Scanner (System,in);
-        Scanner.out.println("Ingrese un numero");
+        Scanner input = new Scanner (System.in);
+        System.out.println("Ingrese un numero");
         int num1 = input.nextInt();
         while (num1 <= 0){
-            Scanner.out.println("Ingrese un numero entero ");
+            System.out.println("Ingrese un numero entero ");
         } 
-        Scanner.out.println("Ingrese otro numero");
+        System.out.println("Ingrese otro numero");
         int num2 = input.nextInt();
         while (num2 <=0){
-            Scanner.out.println("Ingrese otro numero entero ");
-
-            int sum = num1 + num2
-            int rest = num1 - num2
-            int div = num1 / num2
-            int multi = num1 * num2
-
-            Scanner.out.println ("La suma es: "+ sum);
-            Scanner.out.println("La resta es: " + rest);
-            Scanner.out.println("La divisoin es: " + div);
-            Scanner.out.println("La multiplicacion es: " + multi);
+            System.out.println("Ingrese otro numero entero ");
         }
+
+        int sum = num1 + num2;
+        int rest = num1 - num2;
+        int div = num1 / num2;
+        int multi = num1 * num2;
+
+        System.out.println ("La suma es: "+ sum);
+        System.out.println("La resta es: " + rest);
+        System.out.println("La divisoin es: " + div);
+        System.out.println("La multiplicacion es: " + multi);
+        
     }
 }
