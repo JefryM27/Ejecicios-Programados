ddef3669f09e51b226a9ec4954f469c9dcbe4edf
JefryM27 <jefrymoreraespinoza09@gmail.com>
2023-05-22 19:28:48 -0600

    Ejercicio1

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -1,20 +1,14 @@
 import java.util.Scanner;
-
-public class Ejercicio1 {
-
-    public static void public static void main(String[] args) {
-        Scanner entrada = new Scanner(System.in);
-System.out.println ("Ingrese un numero");
-int  num = entrada.nextInt();
-while (num==0){ 
-System.out.println ("Ingrese un numero diferente a cero");
-num = entrada.nextInt();
-} 
-If (num >=0){
-    System.out.println ("El numero es positivo:" + num)
-}
-else {
-    System.out.println ("El numero es negativo:" + num)
-}
+public class EjercicioUno{
+    public static void main(String[] args ){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Ingrese un numero");
+        int num = input.nextInt();
+        if (num%2==0){
+            System.out.println("Par");
+        }else{
+           System.out.println("Impar");
+        }
     }
+
 }
